6eb0359dc8dc1bbd951a1247b74820c6863fbacc
apple <odile_l@126.com>
2019-07-24 10:50:02 +0800

    2.3.3_myLogout

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     #path('login/',views.user_login,name='user_login'),
     path('login/',auth_views.LoginView.as_view(template_name='account/login3.html'),name='user_login'),
     #基于类的方式auth_views.LoginView.as_view()
+
+    path('logout/',auth_views.LogoutView.as_view(template_name='account/logout.html'),name='user_logout'),
 ]
